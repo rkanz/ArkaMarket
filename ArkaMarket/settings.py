c77bd639e1bc540f9a14d1d3786dedb5e8ecfa01
Arka <rkanazar@gmail.com>
2026-05-31 16:31:34 +0430

    Add Project Screenshots

--- ArkaMarket/settings.py
+++ ArkaMarket/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG=os.getenv('DEBUG') == 'True'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
